9bb3ebf13a3b54e01b5a0d78b9e3cf6aefc74cee
ericktorres1 <ericktorres90.et@gmail.com>
2015-03-22 02:14:29 -0400

    first commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Erick Torres on 3/22/15.
+//  Copyright (c) 2015 Erick Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Erick Torres on 3/22/15.
+//  Copyright (c) 2015 Erick Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
