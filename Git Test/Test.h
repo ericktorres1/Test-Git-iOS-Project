4e32ebb88847abaf595a67a145ca978dbdc9e1b2
ericktorres1 <ericktorres90.et@gmail.com>
2015-03-22 04:25:52 -0400

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Erick Torres on 3/22/15.
+//  Copyright (c) 2015 Erick Torres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Erick Torres on 3/22/15.
+//  Copyright (c) 2015 Erick Torres. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
